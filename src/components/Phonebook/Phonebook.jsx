badb3814389ba8bb0429abace1d430122f8d34a7
SebastianZasadni <emblelele@gmail.com>
2023-06-28 09:55:14 +0200

    fix

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,17 +1,14 @@
 import css from './Phonebook.module.css';
 import { ContactsForm } from '../ContactsForm/ContactsForm';
 import { ContactsList } from '../ContactsList/ContactsList';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
-import { getContacts } from 'redux/selectors';
 
 export const Phonebook = () => {
-  const contacts = useSelector(getContacts);
-
+ 
   return (
     <div className={css.sectionPhonebook}>
       <h1>Phonebook</h1>
       <ContactsForm />
-      {contacts.length !== 0 && <ContactsList />}
+      <ContactsList /> 
     </div>
   );
 };
